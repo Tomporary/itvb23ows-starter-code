f801441e82199b4b5d9d21b1f95bb1a098831af6
Tom <152295865+Tomporary@users.noreply.github.com>
2024-03-29 00:26:12 +0100

    Create PlayerActionTest.php

---
+++ app/tests/PlayerActionTest.php
@@ -0,0 +1,120 @@
+<?php
+
+
+use main\DatabaseHandler;
+use main\HiveGame;
+use main\PlayerAction;
+use PHPUnit\Framework\TestCase;
+
+class PlayerActionTest extends TestCase
+{
+    public function testMove_Beetle_NoError()
+    {
+        session_start();
+        
+        $db = new DatabaseHandler();
+        new HiveGame($db);
+        $playerAction = new PlayerAction($db);
+
+        $_SESSION['board']['0,0'] = [[0, 'Q']];
+        $_SESSION['board']['0,1'] = [[1, 'Q']];
+        $_SESSION['board']['-1,0'] = [[0, 'B']];
+        $_SESSION['board']['0,2'] = [[1, 'B']];
+
+        $_SESSION['player'] = 1;
+        $_POST['from'] = '0,2';
+        $_POST['to'] = '0,1';
+
+        $playerAction->move();
+
+        $this->assertFalse(isset($_SESSION['error']), 'Test failed: Beetle move error \''.$_SESSION['error'].'\'');
+    }
+
+    public function testMove_Beetle_CorrectPlacement()
+    {
+        session_start();
+        
+        $db = new DatabaseHandler();
+        new HiveGame($db);
+        $playerAction = new PlayerAction($db);
+
+        $_SESSION['board']['0,0'] = [[0, 'Q']];
+        $_SESSION['board']['0,1'] = [[1, 'Q']];
+        $_SESSION['board']['-1,0'] = [[0, 'B']];
+        $_SESSION['board']['0,2'] = [[1, 'B']];
+
+        $_SESSION['player'] = 1;
+        $_POST['from'] = '0,2';
+        $_POST['to'] = '0,1';
+
+        $playerAction->move();
+
+        $this->assertEquals('B', $_SESSION['board']['0,1'][1], 'Test failed: Beetle move placement');
+    }
+
+    public function testPass() 
+    {
+        session_start();
+        
+        $db = new DatabaseHandler();
+        new HiveGame($db);
+        $playerAction = new PlayerAction($db);
+        $state = $db->getState();
+        $_SESSION['last_move'];
+
+        $stmt = $db->getConnection()->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "play", ?, ?, ?, ?)');
+        $stmt->bind_param('issis', $_SESSION['game_id'], 'Q', '0,1', $_SESSION['last_move'], $state);
+        $stmt->execute();
+        $stmt = $db->getConnection()->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "play", ?, ?, ?, ?)');
+        $stmt->bind_param('issis', $_SESSION['game_id'], 'Q', '1,0', $_SESSION['last_move'], $state);
+        $stmt->execute();
+
+        $playerAction->pass();
+
+        $hist = $db->getMoveHistory();
+
+        $this->assertEquals('pass', array_pop(hist)[2], 'Test failed: Pass()');
+    }
+
+    public function testPlay() 
+    {
+        session_start();
+        
+        $db = new DatabaseHandler();
+        new HiveGame($db);
+        $playerAction = new PlayerAction($db);
+
+        $_POST['piece'] = 'Q';
+        $_POST['to'] = '0,1';
+
+        $playerAction->play();
+
+        $this->assertEquals('Q', $_SESSION['board']['0,1'][0], 'Test failed: Play');
+    }
+
+    public function testRestart() 
+    {
+        session_start();
+        
+        $db = new DatabaseHandler();
+        new HiveGame($db);
+        $playerAction = new PlayerAction($db);
+        $state = $db->getState();
+        $_SESSION['last_move'];
+
+        $stmt = $db->getConnection()->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "play", ?, ?, ?, ?)');
+        $stmt->bind_param('issis', $_SESSION['game_id'], 'Q', '0,1', $_SESSION['last_move'], $state);
+        $stmt->execute();
+        $stmt = $db->getConnection()->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "play", ?, ?, ?, ?)');
+        $stmt->bind_param('issis', $_SESSION['game_id'], 'Q', '1,0', $_SESSION['last_move'], $state);
+        $stmt->execute();
+
+        $playerAction->restart();
+
+        $hist = $db->getMoveHistory();
+
+        $this->assertEmpty($hist, 'Test failed: Restart');
+    }
+
+    // public function testUndo() {}
+}
